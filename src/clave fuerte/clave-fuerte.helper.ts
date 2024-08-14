faaa574a9c6d8c46e5f744dfc601c6c720999668
Guste Gaubaite <219.guste@gmail.com>
2024-08-14 10:43:15 +0200

    Implementada función que verifica si la clave tiene mayúsculas y minúsculas

--- src/clave fuerte/clave-fuerte.helper.ts
+++ src/clave fuerte/clave-fuerte.helper.ts
@@ -3,3 +3,31 @@ import { ValidacionClave } from "./model";
 let validacionClave: ValidacionClave = {
   esValida: false,
 };
+
+export const tieneMayusculasYMinusculas = (clave: string): ValidacionClave => {
+  let tieneMayusculas = false;
+  let tieneMinusculas = false;
+
+  for (let i = 0; i < clave.length; i++) {
+    if (
+      clave[i] === clave[i].toUpperCase() &&
+      clave[i] !== clave[i].toLowerCase()
+    ) {
+      tieneMayusculas = true;
+    } else if (
+      clave[i] === clave[i].toLowerCase() &&
+      clave[i] !== clave[i].toUpperCase()
+    ) {
+      tieneMinusculas = true;
+    }
+
+    if (tieneMayusculas && tieneMinusculas) {
+      validacionClave = {
+        esValida: true,
+      };
+    } else {
+      validacionClave.error = "La clave debe de tener mayúsculas y minúsculas";
+    }
+  }
+  return validacionClave;
+};
