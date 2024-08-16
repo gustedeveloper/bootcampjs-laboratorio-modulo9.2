fc9658569240457b9747cceb3e8840614706b2ff
Guste Gaubaite <219.guste@gmail.com>
2024-08-16 11:27:14 +0200

    Refactor: optimizada función principal

--- src/validar-clave.ts
+++ src/validar-clave.ts
@@ -16,43 +16,22 @@ export const validarClave = (
   clave: string,
   commonPasswords: string[]
 ): ValidacionClave => {
-  let validacionClave: ValidacionClave = {
-    esValida: false,
-  };
+  const validaciones = [
+    tieneMayusculasYMinusculas(clave),
+    tieneNumeros(clave),
+    tieneCaracteresEspeciales(clave),
+    tieneLongitudMinima(clave),
+    tieneNombreUsuario(nombreUsuario, clave),
+    tienePalabrasComunes(clave, commonPasswords),
+  ];
 
-  const tieneMayusYMinus = tieneMayusculasYMinusculas(clave);
-  const tieneNum = tieneNumeros(clave);
-  const tieneCaracterEspecial = tieneCaracteresEspeciales(clave);
-  const tieneLongMinima = tieneLongitudMinima(clave);
-  const tieneNomUsuario = tieneNombreUsuario(nombreUsuario, clave);
-  const tienePalabraComun = tienePalabrasComunes(clave, commonPasswords);
-
-  if (
-    tieneMayusYMinus.esValida === true &&
-    tieneNum.esValida === true &&
-    tieneCaracterEspecial.esValida === true &&
-    tieneLongMinima.esValida === true &&
-    tieneNomUsuario.esValida === true &&
-    tienePalabraComun.esValida === true
-  ) {
-    validacionClave = {
-      esValida: true,
-    };
-  } else if (tieneMayusYMinus.esValida !== true) {
-    validacionClave.error = tieneMayusYMinus.error;
-  } else if (tieneNum.esValida !== true) {
-    validacionClave.error = tieneNum.error;
-  } else if (tieneCaracterEspecial.esValida !== true) {
-    validacionClave.error = tieneCaracterEspecial.error;
-  } else if (tieneLongMinima.esValida !== true) {
-    validacionClave.error = tieneLongMinima.error;
-  } else if (tieneNomUsuario.esValida !== true) {
-    validacionClave.error = tieneNomUsuario.error;
-  } else if (tienePalabraComun.esValida !== true) {
-    validacionClave.error = tienePalabraComun.error;
+  for (const validacion of validaciones) {
+    if (!validacion.esValida) {
+      return validacion;
+    }
   }
 
-  return validacionClave;
+  return { esValida: true };
 };
 
 console.log(validarClave("password", "password", commonPasswords));
@@ -60,6 +39,8 @@ console.log(validarClave("passwordD", "passwordD", commonPasswords));
 console.log(validarClave("passwordD3", "passwordD3", commonPasswords));
 console.log(validarClave("passwordD3@", "passwordD3@", commonPasswords));
 console.log(validarClave("PEPE", "password3D_", commonPasswords));
+console.log(validarClave("PEPE", "password3D_PEPE", commonPasswords));
+console.log(validarClave("PEPE", "passwordPEPE3D_", commonPasswords));
 console.log(validarClave("PEPE", "aB12@_", commonPasswords));
 console.log(validarClave("PEPE", "pepito3D@", commonPasswords));
 console.log(validarClave("PEPE", "pepe3D@s", commonPasswords));
